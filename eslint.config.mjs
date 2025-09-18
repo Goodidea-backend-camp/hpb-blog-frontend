bace3ad9057bb6c1ac5a374db4054b3be90e31fc
linporu <qazwsx123456guy@gmail.com>
2025-09-19 04:43:11 +0800

    chore(eslint): integrate eslint-config-prettier for formatting compatibility - add eslint-config-prettier package to dependencies - include eslint-config-prettier in eslint configuration with Nuxt setup - remove custom vue/html-self-closing rule in favor of prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
-export default withNuxt({
-  rules: {
-    'vue/html-self-closing': 'off'
-  }
-})
+export default withNuxt(
+  {
+    // Custom config here
+  },
+  eslintConfigPrettier
+)
