66363dd3bdf14e11011584222395502d71a41a31
linporu <qazwsx123456guy@gmail.com>
2025-09-19 13:54:06 +0800

    style(admin/index.vue): move template block below script setup - repositioned the <template> block after the <script setup> block for consistency

--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -1,11 +1,11 @@
-<template>
-  <div>
-    <p>content: admin</p>
-  </div>
-</template>
-
 <script setup>
 definePageMeta({
   layout: 'admin'
 })
 </script>
+
+<template>
+  <div>
+    <p>content: admin</p>
+  </div>
+</template>
