c1008ac8939e11c0f9aa65c0c9282e2334fc33de
linporu <qazwsx123456guy@gmail.com>
2025-10-09 17:48:42 +0800

    chore(tinymce): downgrade TinyMCE and Vue integration versions - Downgraded @tinymce/tinymce-vue from 6.3.0 to 4.0.7 - Downgraded tinymce from 6.8.6 to 5.10.9 - Updated import and configuration to accommodate older TinyMCE version - Removed 'code' plugin from toolbar plugins list - Added explicit TinyMCE skin and content CSS settings to config

--- app/pages/admin/new-article.vue
+++ app/pages/admin/new-article.vue
@@ -6,6 +6,36 @@ import { useForm } from 'vee-validate'
 import { toTypedSchema } from '@vee-validate/zod'
 import * as z from 'zod'
 import { Loader2 } from 'lucide-vue-next'
+
+// Import TinyMCE core first
+import 'tinymce/tinymce'
+
+// Import TinyMCE theme and icons
+import 'tinymce/themes/silver'
+import 'tinymce/icons/default'
+
+// Import TinyMCE skin
+import 'tinymce/skins/ui/oxide/skin.css'
+
+// Import essential plugins
+import 'tinymce/plugins/advlist'
+import 'tinymce/plugins/autolink'
+import 'tinymce/plugins/lists'
+import 'tinymce/plugins/link'
+import 'tinymce/plugins/image'
+import 'tinymce/plugins/charmap'
+import 'tinymce/plugins/preview'
+import 'tinymce/plugins/anchor'
+import 'tinymce/plugins/searchreplace'
+import 'tinymce/plugins/visualblocks'
+import 'tinymce/plugins/code'
+import 'tinymce/plugins/fullscreen'
+import 'tinymce/plugins/insertdatetime'
+import 'tinymce/plugins/media'
+import 'tinymce/plugins/table'
+import 'tinymce/plugins/help'
+import 'tinymce/plugins/wordcount'
+
 import Editor from '@tinymce/tinymce-vue'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
@@ -78,13 +108,14 @@ const tinymceConfig = {
     'insertdatetime',
     'media',
     'table',
-    'code',
     'help',
     'wordcount'
   ],
   toolbar:
     'undo redo | blocks | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
-  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+  content_css: false,
+  skin: false
 }
 
 // Track publishMode for conditional rendering
